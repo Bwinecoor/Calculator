9c6900e4ce0e61ccd71e2bfe80bd5a71160d4e4a
Brandon Winecoor <brandonwinecoor@gmail.com>
2023-10-03 13:28:36 -0400

    copy and paste

--- script.js
+++ script.js
@@ -28,11 +28,15 @@ function clearButPress() {
     let decimalClicked = false
     document.getElementById('entry').value = '0'
 }
-
+//store the current value in entry in valMemRestored
 function copyButPress() {
-
+   valMemStored = document.getElementById('entry').value
 }
-
+// if a value has been stored, paste it in the #entry window and assign it as the newVal
 function pasteButPress() {
+    if(valMemStored) {
+        document.getElementById('entry').value = valMemStored
+        newVal = valMemStored
 
+    }
 }
