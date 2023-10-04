e0442792ea5746052ff4a1587e751d85d06bf9d5
Brandon Winecoor <brandonwinecoor@gmail.com>
2023-10-04 14:50:55 -0400

    when pressed, buttons now show up on the display

--- script.js
+++ script.js
@@ -9,8 +9,29 @@ let decimalClicked = false
 let valMemStored = ''
 
 function numButPress(num) {
+    // check if a number has already been put
+    if (resultVal) {
+        //start a new number
+        newVal = num
+        //reset to create a new result
+        resultVal = ''
+    } else {
+        if (num === '.'){
+            if(decimalClicked != true) {
+                //takes the current value of new val and add the character pressed
+                newVal += num
+                //Sets decimal check var to true, wont allow more
+                decimalClicked = true
+            }
+        }else{
+            newVal += num
+            console.log(newVal)
+        }
+    }
+    //update the display
+    document.getElementById('entry').value = newVal
+}
 
-} 
 
 function mathButPress(operator) {
 
@@ -21,20 +42,20 @@ function equalButPress() {
 }
 //clears everything but memory
 function clearButPress() {
-    let prevVal = ''
-    let newVal = ''
-    let resultVal = ''
-    let mathOperator = ''
-    let decimalClicked = false
+    preVal = ''
+    newVal = ''
+    resultVal = ''
+    mathOperator = ''
+    decimalClicked = false
     document.getElementById('entry').value = '0'
 }
 //store the current value in entry in valMemRestored
 function copyButPress() {
-   valMemStored = document.getElementById('entry').value
+    valMemStored = document.getElementById('entry').value
 }
 // if a value has been stored, paste it in the #entry window and assign it as the newVal
 function pasteButPress() {
-    if(valMemStored) {
+    if (valMemStored) {
         document.getElementById('entry').value = valMemStored
         newVal = valMemStored
 
