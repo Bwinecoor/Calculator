f957ecf4fbbd6f8ace9106f154c4d5a9f55f8579
Bwinecoor <123968562+Bwinecoor@users.noreply.github.com>
2023-10-06 12:27:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -34,11 +34,66 @@ function numButPress(num) {
 
 
 function mathButPress(operator) {
+// Check if there was a previous calculation
+  // by seeing if resultVal has a value
 
+  // If result doesn't have a value then store
+  // the current val as a previous for the next
+  // calculation
+  if (!resultVal) {
+    prevVal = newVal
+  } else {
+    // If there is a current result store that as
+    // the previous value entered
+    prevVal = resultVal
+  }
+
+  // Restart creation of new number
+  newVal = ''
+  // Reset decimalClicked
+  decimalClicked = false
+  // Store operator clicked
+  mathOperator = operator
+
+  // Prepare entry for receiving new numbers
+  resultVal = ''
+  document.getElementById('entry').value = ''
 }
 
 function equalButPress() {
+// Reset decimalClicked
+decimalClicked = false
 
+// Convert string numbers to floats
+prevVal = parseFloat(prevVal)
+newVal = parseFloat(newVal)
+
+// Perform calculations based on stored operator
+switch (mathOperator) {
+  case '+':
+    resultVal = prevVal + newVal
+    break
+  case '-':
+    resultVal = prevVal - newVal
+    break
+  case '*':
+    resultVal = prevVal * newVal
+    break
+  case '/':
+    resultVal = prevVal / newVal
+    break
+  // If equals is hit without an operator
+  // leave everything as is
+  default:
+    resultVal = newVal
+}
+
+// Store the current value as the previous so that
+// I can except to next value in the calculation
+prevVal = resultVal
+
+// Put the calculation result in the entry window
+document.getElementById('entry').value = resultVal
 }
 //clears everything but memory
 function clearButPress() {
